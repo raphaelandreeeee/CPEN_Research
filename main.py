b348665d30e91d411214f1dd8c595bab0a6a34c2
raphaelandreeeee <raphaelandrei.lontoc@cvsu.edu.ph>
2024-12-26 01:18:40 +0800

    Refactor main app structure and add new dashboard layout

--- main.py
+++ main.py
@@ -1,44 +1,24 @@
-from kivy.app import App
-from kivy.metrics import dp
-from kivy.uix.behaviors import TouchRippleBehavior
-from kivy.uix.button import Button
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.list import MDList, ThreeLineListItem
+from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.utils import get_color_from_hex
 
 
-class RectangleFlatButton(TouchRippleBehavior, Button):
-    primary_color = get_color_from_hex("#EB8933")
+class Dashboard(Screen):
 
-    def on_touch_down(self, touch):
-        collide_point = self.collide_point(touch.x, touch.y)
-        if collide_point:
-            touch.grab(self)
-            self.ripple_show(touch)
-            return True
-        return False
-
-    def on_touch_up(self, touch):
-        if touch.grab_current is self:
-            touch.ungrab(self)
-            self.ripple_fade()
-            return True
-        return False
+    pass
 
 
-class MyApp(App):
+class TapAttend(MDApp):
+
     def build(self):
-        screen = Builder()
-        screen.add_widget(
-            RectangleFlatButton(
-                text="Hello, World",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-                size_hint=(None, None),
-                size=(dp(110), dp(35)),
-                ripple_color=(0.8, 0.8, 0.8, 0.5),
-            )
-        )
-        return screen
-
+        return Dashboard()
+    
 
 if __name__ == '__main__':
-    MyApp().run()
+
+    TapAttend().run()
+
